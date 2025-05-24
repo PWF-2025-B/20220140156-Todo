00ebedf4f63630a7fa2aff9a4d7bd0704de3f9a7
lenovo <nico.dwi.ft22@mail.umy.c.id>
2025-05-24 15:08:27 +0700

    mailtrap

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::middleware('auth')->group(function () {
+Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
@@ -35,7 +35,6 @@ Route::middleware(['auth', 'admin'])->group(function () {
     Route::delete('/user/{user}', [UserController::class, 'destroy'])->name('user.destroy');
 });
 
-// Category
 Route::resource('category', CategoryController::class);
 
 
